cc27ebc40932d52ca6021e89f42515d1eb798a0e
Stephen Ficklin <spficklin@gmail.com>
2018-12-02 18:05:50 -0800

    Added TripalTestSuite integration

---
+++ tests/TripalGalaxyAPITest.php
@@ -0,0 +1,19 @@
+<?php
+namespace Tests;
+
+use StatonLab\TripalTestSuite\DBTransaction;
+use StatonLab\TripalTestSuite\TripalTestCase;
+
+class TripalGalaxyAPITest extends TripalTestCase {
+  // Uncomment to auto start and rollback db transactions per test method.
+  // use DBTransaction;
+
+  /**
+   * Basic test example.
+   * Tests must begin with the word "test".
+   * See https://phpunit.readthedocs.io/en/latest/ for more information.
+   */
+  public function testBasicExample() {
+    $this->assertTrue(true);
+  }
+}
